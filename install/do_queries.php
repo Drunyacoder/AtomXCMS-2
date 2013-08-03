6308e04957ff3853c58a5e29bcf7eec4ccaf6cc6
drunyacoder <drunyacoder@gmail.com>
2013-08-03 21:56:57 +0300

    premoderation fix in install

--- install/do_queries.php
+++ install/do_queries.php
@@ -92,14 +92,18 @@ $array[] = "DROP TABLE IF EXISTS `{$set['db']['prefix']}pages`";
 $array[] = "CREATE TABLE `{$set['db']['prefix']}pages` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) collate utf8_general_ci NOT NULL,
+  `title` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `template` varchar(255) default '' collate utf8_general_ci NOT NULL,
   `content` longtext collate utf8_general_ci NOT NULL,
   `url` varchar(255) default ''  NOT NULL,
+  `meta_title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `meta_keywords` varchar(255) default ''  NOT NULL,
   `meta_description` text,
   `parent_id` int(11) default 0  NOT NULL,
   `path` varchar(255) default '1.'  NOT NULL,
   `visible` enum('1','0') default '1'  NOT NULL,
+  `publish` ENUM( '0', '1' ) NOT NULL DEFAULT '1',
+  `position` INT( 11 ) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 $array[] = "INSERT INTO `{$set['db']['prefix']}pages` (`id`,`name`,`path`,`content`) VALUES ('1', 'root', '.', '')";
@@ -128,6 +132,7 @@ $array[] = "CREATE TABLE `{$set['db']['prefix']}loads` (
   `available` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `view_on_home` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `on_home_top` ENUM( '0', '1' ) DEFAULT '0' NOT NULL,
+  `premoder` ENUM( 'nochecked', 'rejected', 'confirmed' ) NOT NULL DEFAULT 'confirmed',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 #####################################################################
@@ -189,6 +194,7 @@ $array[] = "CREATE TABLE `{$set['db']['prefix']}news` (
   `available` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `view_on_home` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `on_home_top` ENUM( '0', '1' ) DEFAULT '0' NOT NULL,
+  `premoder` ENUM( 'nochecked', 'rejected', 'confirmed' ) NOT NULL DEFAULT 'confirmed',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 $array[] = "INSERT INTO `{$set['db']['prefix']}news` VALUES (1, 'Моя первая новость', 'Теперь сайт установлен и вы можете приступать его настройке. По любым вопросам обращайтесь на официальный сайт Fapos.net', 0, NOW(), 1, 1, 0, '', '', '', '', '', '1', '1', '1', '0')";
@@ -249,6 +255,7 @@ $array[] = "CREATE TABLE `{$set['db']['prefix']}stat` (
   `available` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `view_on_home` ENUM( '0', '1' ) DEFAULT '1' NOT NULL,
   `on_home_top` ENUM( '0', '1' ) DEFAULT '0' NOT NULL,
+  `premoder` ENUM( 'nochecked', 'rejected', 'confirmed' ) NOT NULL DEFAULT 'confirmed',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 #####################################################################
