f46890b2c94b07130bbdee94293b0dd3e3bafecf
drunyacoder <drunyacoder@gmail.com>
2014-03-09 21:41:41 +0200

    fix snippets

---
+++ sys/inc/ORM/Entities/Snippets.php
@@ -0,0 +1,56 @@
+<?php
+/*---------------------------------------------\
+|											   |
+| @Author:       Andrey Brykin (Drunya)        |
+| @Version:      1.3                           |
+| @Project:      CMS                           |
+| @Package       AtomX CMS                     |
+| @subpackege    Snippets Entity               |
+| @copyright     ©Andrey Brykin 2010-2014      |
+| @last mod      2014/03/09                    |
+|----------------------------------------------|
+|											   |
+| any partial or not partial extension         |
+| CMS Fapos,without the consent of the         |
+| author, is illegal                           |
+|----------------------------------------------|
+| Любое распространение                        |
+| CMS Fapos или ее частей,                     |
+| без согласия автора, является не законным    |
+\---------------------------------------------*/
+
+
+
+/**
+ *
+ */
+class SnippetsEntity extends FpsEntity
+{
+	
+	protected $id;
+	protected $name;
+	protected $body;
+	
+	
+	public function save()
+	{
+		$params = array(
+			'name' => $this->ips,
+			'body' => $this->cookie,
+		);
+		
+		if ($this->id) $params['id'] = $this->id;
+		$Register = Register::getInstance();
+		return $Register['DB']->save('snippets', $params);
+	}
+	
+	
+	
+	public function delete()
+	{ 
+		$Register = Register::getInstance();
+		$Register['DB']->delete('snippets', array('id' => $this->id));
+	}
+
+
+}
---
+++ sys/inc/ORM/Models/Snippets.php
@@ -0,0 +1,50 @@
+<?php
+/*---------------------------------------------\
+|											   |
+| @Author:       Andrey Brykin (Drunya)        |
+| @Version:      1.0                           |
+| @Project:      CMS                           |
+| @Package       AtomX CMS                     |
+| @subpackege    Snippets Model                |
+| @copyright     ©Andrey Brykin 2010-2014      |
+| @last mod      2014/03/09                    |
+|----------------------------------------------|
+|											   |
+| any partial or not partial extension         |
+| CMS Fapos,without the consent of the         |
+| author, is illegal                           |
+|----------------------------------------------|
+| Любое распространение                        |
+| CMS Fapos или ее частей,                     |
+| без согласия автора, является не законным    |
+\---------------------------------------------*/
+
+
+
+/**
+ *
+ */
+class SnippetsModel extends FpsModel
+{
+	public $Table = 'snippets';
+
+    protected $RelatedEntities = array(
+    );
+
+	public function getByName($name)
+	{
+        $Register = Register::getInstance();
+		$entity = $this->getDbDriver()->select($this->Table, DB_FIRST, array(
+			'cond' => array(
+				'name' => $name
+			)
+		));
+
+		if (!empty($entity[0])) {
+			$entityClassName = $Register['ModManager']->getEntityNameFromModel(get_class($this));
+			$entity = new $entityClassName($entity[0]);
+			return (!empty($entity)) ? $entity : false;
+		}
+		return false;
+	}
+}
