44c61386143b0a950c7f2d8b08d058d36acdb79a
Drunyacoder <drunya131@rambler.ru>
2014-04-21 07:55:48 +0300

    fix premoderation in loads

--- modules/loads/index.php
+++ modules/loads/index.php
@@ -38,6 +38,8 @@ Class LoadsModule extends Module {
 	*/
 	public $module = 'loads';
 	
+	public $premoder_types = array('rejected', 'confirmed');
+	
 	/**
 	* @module module indentifier
 	*/
@@ -1388,7 +1390,31 @@ Class LoadsModule extends Module {
 		header('Location: ' . $entity->getDownload_url());
 	}
 	
+	
+	
+	/**
+	* @param int $id - record ID
+	*
+	* fix or unfix record on top on home page
+	*/
+	public function premoder($id, $type)
+    {
+		$this->ACL->turn(array('other', 'can_premoder'));
+		$id = (int)$id;
+		if ($id < 1) redirect('/' . $this->module . '/');
+		
+		if (!in_array((string)$type, $this->premoder_types)) 
+			return $this->showInfoMessage(__('Some error occurred'), '/' . $this->module . '/');
 
+		$target = $this->Model->getById($id);
+		if (!$target) redirect('/');
+		
+		$target->setPremoder((string)$type);
+		$target->save();
+		return $this->showInfoMessage(__('Operation is successful'), '/' . $this->module . '/');
+	}
+	
+	
 	
 	/**
 	* @param array $record - assoc record array
@@ -1402,6 +1428,30 @@ Class LoadsModule extends Module {
         $uid = $record->getAuthor_id();
         $id = $record->getId();
 
+		if ($this->ACL->turn(array('other', 'can_premoder'), false) && 'nochecked' == $record->getPremoder()) {
+			$moder_panel .= get_link('', '/' . $this->module . '/premoder/' . $id . '/confirmed',
+				array(
+					'class' => 'fps-premoder-confirm', 
+					'title' => 'Confirm', 
+					'onClick' => "return confirm('" . __('Are you sure') . "')",
+				)) . '&nbsp;';
+			$moder_panel .= get_link('', '/' . $this->module . '/premoder/' . $id . '/rejected',
+				array(
+					'class' => 'fps-premoder-reject', 
+					'title' => 'Reject', 
+					'onClick' => "return confirm('" . __('Are you sure') . "')",
+				)) . '&nbsp;';
+		
+		
+		} else if ($this->ACL->turn(array('other', 'can_premoder'), false) && 'rejected' == $record->getPremoder()) {
+			$moder_panel .= get_link('', '/' . $this->module . '/premoder/' . $id . '/confirmed',
+				array(
+					'class' => 'fps-premoder-confirm', 
+					'title' => 'Confirm', 
+					'onClick' => "return confirm('" . __('Are you sure') . "')",
+				)) . '&nbsp;';
+		}
+		
 
 		if ($this->ACL->turn(array($this->module, 'edit_materials'), false) 
 		|| (!empty($_SESSION['user']['id']) && $uid == $_SESSION['user']['id']
